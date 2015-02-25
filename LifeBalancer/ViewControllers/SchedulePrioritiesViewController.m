d026d20a710f29c503f6527ccf3f147c87232708
Alex Czartoryski <alex@czarto.com>
2015-02-25 02:46:58 -0500

    Removed old code
    Removed old calendar scheduling code

--- LifeBalancer/ViewControllers/SchedulePrioritiesViewController.m
+++ LifeBalancer/ViewControllers/SchedulePrioritiesViewController.m
@@ -10,7 +10,6 @@
 #import "DataAdapter.h"
 #import "Role.h"
 #import "Task.h"
-#import "ScheduleTaskViewController.h"
 
 @interface SchedulePrioritiesViewController ()<UITabBarDelegate>
 {
@@ -20,28 +19,9 @@
 
 @implementation SchedulePrioritiesViewController
 
-//- (id)initWithStyle:(UITableViewStyle)style
-//{
-//    self = [super initWithStyle:style];
-//    if (self) {
-//        // Custom initialization
-//    }
-//    return self;
-//}
-
 -(void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
      self.navigationItem.title = @"Schedule Priorities";
-
-    //creat barbuttons
-//    UIBarButtonItem *backButton = [[ UIBarButtonItem alloc ] initWithTitle: @"Home"
-//                                                                     style: UIBarButtonItemStyleBordered
-//                                                                    target: self
-//                                                                    action: @selector(home)];
-//    
-	
-     // self.toolbarItems = [ NSArray arrayWithObjects: backButton,flexibleSpace,doneButton,nil];
-//     self.navigationItem.leftBarButtonItem = backButton;
       [self loadroles];
 }
 
@@ -114,10 +94,6 @@
             formatString = @"\u273A  %@";
         }
         
-//        if (self.removeMarks) {
-//            formatString = @"\u273A  %@";
-//        }
-        
         NSString *completeString = [NSString stringWithFormat:formatString,task.name];
         swipedCell.textLabel.text = completeString;
     }
@@ -146,18 +122,8 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-  /*  Role *role = rolesarray[section];
-    
-    DataAdapter *s = [[DataAdapter alloc]init];
-    NSMutableArray *goalsarray = [NSMutableArray arrayWithArray:[s selectedGoalsInSelectedRole:[role objectID]]];
-    
-    return goalsarray.count;*/
-    
-    
     Role *role = rolesarray[section];
     return [role.tasks count];
-    
-    
 }
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
@@ -192,52 +158,11 @@
        formatString = @"\u273A  %@";
     }
     
-//    if (self.removeMarks) {
-//        formatString = @"\u273A  %@";
-//    }
-    
      NSString *completeString = [NSString stringWithFormat:formatString,task.name];
     rolecell.textLabel.text = completeString;
 
     return rolecell;
 }
-
-
-
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
-    
-    [self tableCellSelect:indexPath];
-}
-
-- (void)tableView:(UITableView *)tableView accessoryButtonTappedForRowWithIndexPath:(NSIndexPath *)indexPath{
-
-    [self tableCellSelect:indexPath];
-}
-
-
--(void)tableCellSelect:(NSIndexPath *)indexPath {
-
-    ScheduleTaskViewController *scheduleTaskViewController = nil;
-    
-    scheduleTaskViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"ScheduleTask"];
-    
-    
-    Role *role = rolesarray[indexPath.section];
-    
-    DataAdapter *da = [[DataAdapter alloc]init];
-    NSMutableArray *tasksarray = [NSMutableArray arrayWithArray:[da getTasksByPriority:role.objectID]];
-    
-    
-    scheduleTaskViewController.role = role;
-    scheduleTaskViewController.task = [tasksarray objectAtIndex:indexPath.row];
-    
-    
-    if (scheduleTaskViewController) {
-        [self.navigationController pushViewController:scheduleTaskViewController animated:YES];
-    }
-
-}
-
 
 #pragma mark -
 #pragma mark Buttom Bar Buttons
@@ -269,13 +194,6 @@
 
 -(void)tabBar:(UITabBar *)tabBar didSelectItem:(UITabBarItem *)item
 {
-//    NSLog(@"selected item tag : %d",item.tag);
-//    if (item.tag==2) {
-//        [self back];
-//    }
-//    else  if (item.tag==1) {
-//        [self done];
-//    }
     
     NSLog(@"selected item tag : %d",(int)item.tag);
     if (item.tag==1) {
@@ -290,9 +208,6 @@
     else if(item.tag==4)
     {
         GoalViewController *gVc = [self.storyboard instantiateViewControllerWithIdentifier:@"tempGoalVC"];
-        //CGRect rect = CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height-49);
-        // gVc.view.frame = rect;
-        //[self.view addSubview:gVc.view];
         [self.navigationController pushViewController:gVc animated:NO];
     }
 }
